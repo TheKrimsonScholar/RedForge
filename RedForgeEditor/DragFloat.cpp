11d6db1cb5a017fdc2c5e8f527f1f790534e4686
TheKrimsonScholar <133544020+TheKrimsonScholar@users.noreply.github.com>
2025-11-12 23:35:26 -0500

    Cleanup of old files

--- RedForgeEditor/DragFloat.cpp
+++ RedForgeEditor/DragFloat.cpp
@@ -1,1 +1,85 @@
 #include "DragFloat.h"
+
+#include <QDoubleValidator>
+
+DragFloat::DragFloat(float initialValue, QWidget* parent) : QLineEdit(parent)
+{
+    // Handle focus manually
+    setFocusPolicy(Qt::NoFocus);
+    setAlignment(Qt::AlignRight);
+
+    // Only allow numeric input
+    setValidator(new QDoubleValidator(-FLT_MAX, FLT_MAX, 6, this));
+
+    QObject::connect(this, &DragFloat::editingFinished,
+        [this]()
+        {
+            SetValue(text().toFloat());
+        });
+
+    // Initialize value
+    SetValue(initialValue);
+}
+DragFloat::~DragFloat()
+{
+
+}
+
+void DragFloat::focusInEvent(QFocusEvent* event)
+{
+    // Highlight the full value upon focusing in (for quick edits)
+    selectAll();
+
+    QLineEdit::focusInEvent(event);
+}
+
+void DragFloat::mousePressEvent(QMouseEvent* event)
+{
+    isPressed = true;
+
+    pressPosition = event->pos();
+    pressValue = value;
+
+    QLineEdit::mousePressEvent(event);
+}
+void DragFloat::mouseMoveEvent(QMouseEvent* event)
+{
+    // Unfocused state: holding and dragging increments/decrements value
+    if(!hasFocus())
+    {
+        setCursor(Qt::SizeHorCursor);
+
+        if(isPressed)
+        {
+            isDragging = true;
+
+            QPoint delta = event->pos() - pressPosition;
+            SetValue(pressValue + increment * (delta.x() / pixelsPerIncrement));
+        }
+    }
+    // Focused state: manual number input field; QLineEdit handles everything normally
+    else
+    {
+        setCursor(Qt::IBeamCursor);
+
+        QLineEdit::mouseMoveEvent(event);
+    }
+}
+void DragFloat::mouseReleaseEvent(QMouseEvent* event)
+{
+    isPressed = false;
+
+    // Only focus in if not dragging value
+    if(!isDragging)
+        setFocus(Qt::OtherFocusReason);
+        
+    isDragging = false;
+}
+
+void DragFloat::SetValue(float value)
+{
+    this->value = value;
+    setText(QString::number(value, 'f', precision));
+
+    emit valueChanged(value);
+}
--- RedForgeEditor/DragFloat.h
+++ RedForgeEditor/DragFloat.h
@@ -2,7 +2,6 @@
 
 #include <QLineEdit>
 #include <QMouseEvent>
-#include <QDoubleValidator>
 
 class DragFloat : public QLineEdit
 {
@@ -21,87 +20,20 @@ private:
     float value = 0.0f;
 
 public:
-    explicit DragFloat(float initialValue, QWidget* parent = nullptr) : QLineEdit(parent)
-    {
-        // Handle focus manually
-        setFocusPolicy(Qt::NoFocus);
-        setAlignment(Qt::AlignRight);
-
-        // Only allow numeric input
-        setValidator(new QDoubleValidator(-FLT_MAX, FLT_MAX, 6, this));
-
-        QObject::connect(this, &DragFloat::editingFinished,
-            [this]()
-            {
-                SetValue(text().toFloat());
-            });
-        
-        // Initialize value
-        SetValue(initialValue);
-    }
+    explicit DragFloat(float initialValue, QWidget* parent = nullptr);
+    ~DragFloat();
 
 protected:
-    void focusInEvent(QFocusEvent* event) override
-    {
-        // Highlight the full value upon focusing in (for quick edits)
-        selectAll();
+    void focusInEvent(QFocusEvent* event) override;
 
-        QLineEdit::focusInEvent(event);
-    }
-
-    void mousePressEvent(QMouseEvent* event) override
-    {
-        isPressed = true;
-
-        pressPosition = event->pos();
-        pressValue = value;
-
-        QLineEdit::mousePressEvent(event);
-    }
-    void mouseMoveEvent(QMouseEvent* event) override
-    {
-        // Unfocused state: holding and dragging increments/decrements value
-        if(!hasFocus())
-        {
-            setCursor(Qt::SizeHorCursor);
-
-            if(isPressed)
-            {
-                isDragging = true;
-
-                QPoint delta = event->pos() - pressPosition;
-                SetValue(pressValue + increment * (delta.x() / pixelsPerIncrement));
-            }
-        }
-        // Focused state: manual number input field; QLineEdit handles everything normally
-        else
-        {
-            setCursor(Qt::IBeamCursor);
-
-            QLineEdit::mouseMoveEvent(event);
-        }
-    }
-    void mouseReleaseEvent(QMouseEvent* event) override
-    {
-        isPressed = false;
-
-        // Only focus in if not dragging value
-        if(!isDragging)
-            setFocus(Qt::OtherFocusReason);
-        
-        isDragging = false;
-    }
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
 
 public:
     float GetValue() { return value; }
 
-    void SetValue(float value)
-    {
-        this->value = value;
-        setText(QString::number(value, 'f', precision));
-
-        emit valueChanged(value);
-    }
+    void SetValue(float value);
 
 signals:
     void valueChanged(float value);
